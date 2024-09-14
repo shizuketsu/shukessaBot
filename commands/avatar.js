8655309da74b7bde79d80f231a8b7e96450fc79b
Shizu <evdokimovnikita501@gmail.com>
2024-09-14 23:17:06 +0300

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,18 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'avatar',
+    description: 'avatar',
+    async execute(msg, args) {
+        const mentionedUsers = msg.mentions.users;
+        const user = args[0] && mentionedUsers.size > 0 ? mentionedUsers.first() : msg.author;
+
+        const embed = new EmbedBuilder()
+            .setColor('#92a2ff')
+            .setTitle('Avatar of ' + user.username + ':')
+            .setImage(user.displayAvatarURL())
+            .setFooter({ text: 'Requested by ' + msg.author.username, iconURL: msg.author.displayAvatarURL() });
+
+        msg.channel.send({ embeds: [embed] });
+    }
+}
