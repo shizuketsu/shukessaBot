491caa661ab55f4e5d6949c132084766f11ed138
Shizu <evdokimovnikita501@gmail.com>
2024-09-15 11:45:01 +0300

    Delete commands/test.js

--- commands/test.js
+++
@@ -1,9 +0,0 @@
-const discord = require('discord.js');
-
-module.exports = {
-    name: 'test',
-    description: 'test',
-    async execute(msg, args) {
-        msg.reply(args[0] + '|' + args[1] + '|' + args[2]);
-    }
-}
